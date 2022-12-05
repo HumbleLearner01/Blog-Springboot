88921821d20c9fa77d62e55831b857e38b6e88e3
HumbleLearner01 <learn.mike.helloworld@gmail.com>
2022-12-05 12:36:22 +0330

    Security Configuration.

---
+++ src/main/java/com/example/blogpost/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.blogpost.configuration;
+
+import com.example.blogpost.configuration.impl.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((request) -> {
+            try {
+                request.antMatchers("/**").permitAll()
+                        .antMatchers("/user/**").hasRole("USER")
+                        .and().formLogin().loginPage("/login").and().cors().and().csrf().disable();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).authenticationProvider(authenticationProvider());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        return daoAuthenticationProvider;
+    }
+}
